07cd6f0fdbf7852db559190813f68c3e36d35b6a
Mansi Gupta <138883917+mansi9898@users.noreply.github.com>
2024-06-20 11:33:19 +0530

    Update NavigationPage.java

--- src/main/java/pages/NavigationPage.java
+++ src/main/java/pages/NavigationPage.java
@@ -1,67 +1,3 @@
-//package pages;
-//
-//import java.time.Duration;
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.JavascriptExecutor;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.PageFactory;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//
-//public class NavigationPage {
-//    private WebDriver driver;
-//    private WebDriverWait wait;
-//
-//    // Constructor to initialize the WebDriver and WebDriverWait
-//    public NavigationPage(WebDriver driver) {
-//        this.driver = driver;
-//        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15)); // Wait up to 10 seconds
-//        PageFactory.initElements(driver, this);
-//    }
-//
-//    public void selectHomeandFurniture() {
-//        WebElement dropDown = driver.findElement(By.cssSelector("div._1ch8e_"));
-//
-//        // Wait until the element is clickable
-//        wait.until(ExpectedConditions.elementToBeClickable(dropDown));
-//        
-//        // Scroll the element into view if it's not fully visible
-//        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", dropDown);
-//
-//        dropDown.click();
-//    }
-//
-//    public void goToFurnitureStudio() {
-//        WebElement furnitureLink = driver.findElement(By.partialLinkText("Men's T-Shirts"));
-//
-//        // Wait until the element is clickable
-//        wait.until(ExpectedConditions.elementToBeClickable(furnitureLink));
-//        
-//        // Scroll the element into view if it's not fully visible
-//        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", furnitureLink);
-//
-//        furnitureLink.click();
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 package pages;
 
 import java.time.Duration;
@@ -80,16 +16,16 @@ public class NavigationPage {
     // Constructor to initialize the WebDriver and WebDriverWait
     public NavigationPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15)); // Wait up to 15 seconds
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // Wait up to 20 seconds
         PageFactory.initElements(driver, this);
     }
     
     
-    public void selectHomeandFurniture() {
+     public void selectAppliances() {
         // Locate and click on the furniture image link
-        WebElement furnitureImage = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@class='_2puWtW _3a3qyb' and @alt='Furniture']")));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", furnitureImage);
-        furnitureImage.click();
+        WebElement Appliance = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Appliances']")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", Appliance);
+        Appliance.click();
 
         // Scroll to the bottom of the page
         ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
